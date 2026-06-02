93abced28cb64614f9d9ea275feeff3f162895cc
darrel124568 <darrel124568@gmail.com>
2026-06-02 13:45:43 +0300

    initial

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def set_pages(self, page_count):
+        if type(page_count) is int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
     
+    def get_pages(self):
+        return self._page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")  
+
+    page_count = property(get_pages, set_pages)
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+    
+    def set_size(self, size):
+        if size in ["Small", "Medium", "Large"] :
+            self._size = size
+        else:
+            print("size must be Small, Medium, or Large")
+    def get_size(self):
+        return self._size
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")  
+        self.price += 1
+
+    size = property(get_size, set_size)
